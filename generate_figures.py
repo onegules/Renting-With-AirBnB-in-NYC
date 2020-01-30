e41226d2b2716c593278d5f9cc18260794b8147e
Ovi <Chrono.hi@gmail.com>
2020-01-30 14:16:36 -0500

    feat: Add starter code

---
+++ generate_figures.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+import seaborn as sns
+
+def generate_word_count_figure(path_to_save):
+    pass
+
+def generate_room_type_figure(path_to_save):
+    pass
+
+
+def generate_neighbourhood_figure(path_to_save):
+    pass
+
+if __name__ == '__main__':
+    pass
