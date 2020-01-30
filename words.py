8ff7b061b3fd7c46a478f027ffb5c15d077593dc
Ovi <Chrono.hi@gmail.com>
2020-01-30 14:32:09 -0500

    fix: Change name of file

---
+++ words.py
@@ -0,0 +1,45 @@
+from collections import Counter
+import numpy as np
+import nltk
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+nltk.download(['punkt', 'stopwords', 'wordnet'])
+
+def compute_word_counts(messages, load=True, filepath='counts.npz'):
+    '''
+    input: (
+        messages: list or numpy array
+        load: Boolean value if load or run model
+        filepath: filepath to save or load data
+            )
+    Function computes the top 20 words in the dataset with counts of each term
+    output: (
+        top_words: list
+        top_counts: list
+            )
+    '''
+    if load:
+        # load arrays
+        data = np.load(filepath)
+        return list(data['top_words']), list(data['top_counts'])
+    else:
+        # get top words
+        counter = Counter()
+        for message in messages:
+            tokens = tokenize(message)
+            for token in tokens:
+                counter[token] += 1
+        # top 20 words
+        top = counter.most_common(20)
+        top_words = [word[0] for word in top]
+        top_counts = [count[1] for count in top]
+        # save arrays
+        np.savez(filepath, top_words=top_words, top_counts=top_counts)
+        return list(top_words), list(top_counts)
+
+def tokenize(text):
+    pass
+
+if __name__ == '__main__':
+    pass
